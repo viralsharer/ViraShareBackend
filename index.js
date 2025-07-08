57457b5dcc1ed9223e9c9754c3ab0c0b45ab77b3
oladitisodiq <oladitisodiq@email.com>
2025-07-08 15:27:33 +0100

    cron and refereer

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const passport = require('./config/passport');
 const session = require('express-session');
+const cron = require('node-cron');
+const axios = require('axios');
 
 
 
